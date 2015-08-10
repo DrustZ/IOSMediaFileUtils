572596d333a1345aa67f505f7db6408a7f7e3399
张明瑞 <z1m6r3@gmail.com>
2015-08-10 20:25:39 +0800

    Header
    header

---
+++ IOSMediaFileUtils.h
@@ -0,0 +1,21 @@
+//
+//  IOSMediaFileUtils.h
+//  FunCam
+//
+//  Created by 明瑞 on 14/12/22.
+//  Copyright (c) 2014年 明瑞. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface IOSMediaFileUtils : NSObject
+
++ (void) SaveFileAtCurrentTime:(NSURL *)fromUrl;
++ (NSURL*) SaveImageAtCurrentTime:(UIImage*)image;
++ (void) mergeTwoVideo:(NSURL *)url1 anotherURL:(NSURL *)url2 toURL:(NSURL *)tourl;
++ (void)convertVideoToLowQuailtyWithInputURL:(NSURL*)inputURL
+                                   outputURL:(NSURL*)outputURL;
++ (void)convertToMp4:(NSURL*)inputURL
+           outputURL:(NSURL*)outputURL;
+@end
